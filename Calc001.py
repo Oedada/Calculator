972700e4132a48a99ca44756ce682bb1f5a2982a
Oedada <163449434+Oedada@users.noreply.github.com>
2024-03-15 15:02:51 +0300

    Update Calc001.py

--- Calc001.py
+++ Calc001.py
@@ -1,14 +1,15 @@
+import math
 a = 0
 b = 0
 operation = ""
 while operation != "break":
     print("Напишите первое число ",end = '')
     a = int(input())
-    while operation != "+" and operation != "-" and operation != "*" and operation != "/" and operation != "**" and operation != ">":
+    while operation != "+" and operation != "-" and operation != "*" and operation != "/" and operation != "**" and operation != ">" and operation != "sin":
         print("Введите нужную операцию ",end = '')
         operation = input()
-        if operation != "+" and operation != "-" and operation != "*" and operation != "/" and operation != "**" and operation != ">":
-            print("ошибка ввода")
+        if operation != "+" and operation != "-" and operation != "*" and operation != "/" and operation != "**" and operation != ">" and operation != "sin":
+            print("ошибка ввода")    
     print("Напишите второе число ",end = '')
     b = int(input())
     print("Весь список доступных операций доступен на https://github.com/Oedada/Calculator/blob/63b1ab9a9360a04e0378b39d0d510b5910206a62/README.md?plain=1")
@@ -25,6 +26,8 @@ while operation != "break":
         print(a**b)  
     elif operation==">":
         print(a**(1/b)) 
+    elif operation=="sin":
+        print(math.sin(a))    
     operation = ""               
 
 
